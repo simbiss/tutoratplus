a614f674af3505d4ac4edc3e3782e83dd9f0645d
Simon <simonbiss20@gmail.com>
2023-10-28 10:50:50 -0400

    réajustement des interfaces et création du navGraph

--- app/src/main/java/com/appnat3/tutoratplus/MainActivity.kt
+++ app/src/main/java/com/appnat3/tutoratplus/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.appnat3.tutoratplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        }
     }
-}
