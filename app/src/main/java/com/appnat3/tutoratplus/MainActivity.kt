097dd70f0e0301fb1a235e6fa1fc39d59be5732c
Achraf Louragli <1986111@crosemont.qc.ca>
2023-10-28 11:12:08 -0400

    Ajjustement de la structure présentation

--- app/src/main/java/com/appnat3/tutoratplus/MainActivity.kt
+++ app/src/main/java/com/appnat3/tutoratplus/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.appnat3.tutoratplus
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        }
     }
+}
