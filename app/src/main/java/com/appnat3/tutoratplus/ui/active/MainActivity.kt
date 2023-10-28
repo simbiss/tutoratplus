68aaca02f270139018c2e223ce1965d390ddef2b
Achraf Louragli <1986111@crosemont.qc.ca>
2023-10-28 11:46:50 -0400

    Déplacer MainActivity dans le folder ui.active

---
+++ app/src/main/java/com/appnat3/tutoratplus/ui/active/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appnat3.tutoratplus.ui.active
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appnat3.tutoratplus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
