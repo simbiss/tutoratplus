6237f7d352a582baa25296267c32ebf570a08fc6
Achraf Louragli <1986111@crosemont.qc.ca>
2023-10-29 14:01:13 -0400

    Création du fragment InformationPersonnelle

--- app/src/main/java/com/appnat3/tutoratplus/InformationPersonnlle.kt
+++ app/src/main/java/com/appnat3/tutoratplus/InformationPersonnlle.kt
@@ -5,9 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.appnat3.tutoratplus.présentation.présentateur.PrésentateurInfosPersonnelle
+import com.appnat3.tutoratplus.présentation.présentateur.PrésentateurListeCours
 
 
 class InformationPersonnlle : Fragment() {
+    //Déclaration de variables
+    lateinit var navController: NavController
+    lateinit var btnSuivant: Button
+    lateinit var btnRetour: LinearLayout
+    lateinit var btnAcceuil: TextView
+    var présentateur = PrésentateurInfosPersonnelle(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +34,31 @@ class InformationPersonnlle : Fragment() {
         // Inflate the layout for this fragment
          val vue = inflater.inflate(R.layout.fragment_information_personnlle, container, false)
 
+        btnRetour = vue.findViewById(R.id.buttonRetour)
+        btnRetour.setOnClickListener {
+            présentateur.effectuerNaviguationCalendrier()
+        }
+
+        btnAcceuil = vue.findViewById(R.id.textAcceuil)
+        btnAcceuil.setOnClickListener {
+            présentateur.effectuerNaviguationAcceuil()
+        }
         return  vue
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Obtient le NavController pour la navigation
+        navController = Navigation.findNavController(view)
+    }
+
+    fun naviguerVerspage_calendrier() {
+        navController.navigate(R.id.action_informationPersonnlle_to_page_calendrier)
+    }
+
+    fun naviguerVersmenu_principal() {
+        navController.navigate(R.id.action_informationPersonnlle_to_menu_principal2)
     }
 
 }
