972facd0e0fa62063f9824a5bf7395fe33210469
Achraf Louragli <1986111@crosemont.qc.ca>
2023-11-14 18:07:19 -0500

    créer le fichier ModelTest

---
+++ app/src/test/java/com/appnat3/tutoratplus/ModeleTest.kt
@@ -0,0 +1,11 @@
+package com.appnat3.tutoratplus
+import org.junit.Test
+
+import org.junit.Assert.*
+class ModeleTest {
+
+    @Test
+    fun `Étant donné on choisi le cours de Programmation2 alors on btient la liste des tuteurs de ce cours`() {
+
+    }
+}
