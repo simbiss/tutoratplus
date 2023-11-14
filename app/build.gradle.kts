0634e5bedc39013cdbc0110a9c33211b1818e905
Achraf Louragli <1986111@crosemont.qc.ca>
2023-11-14 18:29:14 -0500

    Configurer mockito dans le build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
--- app/src/test/java/com/appnat3/tutoratplus/ModeleTest.kt
+++ app/src/test/java/com/appnat3/tutoratplus/ModeleTest.kt
@@ -1,11 +1,48 @@
 package com.appnat3.tutoratplus
+import com.appnat3.tutoratplus.presentation.Modele
+import com.appnat3.tutoratplus.sourceDeDonnees.SourceDeDonnees
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import org.junit.After
 import org.junit.Test
+import java.util.Scanner
+import java.io.PrintStream
 
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import org.junit.runner.RunWith
 import org.junit.Assert.*
-class ModeleTest {
+import org.junit.Before
 
+@RunWith(MockitoJUnitRunner::class)
+class ModeleTest {
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+/*
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+*/
     @Test
-    fun `Étant donné on choisi le cours de Programmation2 alors on btient la liste des tuteurs de ce cours`() {
+    fun `Lorsqu'on choisi le cours de Programmation2 alors on btient la liste des tuteurs de ce cours`() {
+        // Mise en place
+
+        // Prévoit l'entrée à saisir
+        /*
+        val source : SourceDeDonnees = Mockito.mock( SourceDeDonnees::class.java )
+        Mockito.`when`( source.obtenirlisteTuteur() ).thenReturn(  )
+        val modèle = Modele( source = source )
+
+        val pourcentage_données = modele.
+        assertEquals( 99, pourcentage_données )
+        */
+
 
     }
 }
