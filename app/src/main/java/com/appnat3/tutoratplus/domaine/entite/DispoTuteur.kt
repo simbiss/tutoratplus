60d02b1b9907144ba65e0fa3a471887428e1f2bb
Simon <simonbiss20@gmail.com>
2023-12-07 17:14:41 -0500

    ajout option reservation classe dispoTuteur

--- app/src/main/java/com/appnat3/tutoratplus/domaine/entite/DispoTuteur.kt
+++ app/src/main/java/com/appnat3/tutoratplus/domaine/entite/DispoTuteur.kt
@@ -1,6 +1,6 @@
 package com.appnat3.tutoratplus.domaine.entite
 
-class DispoTuteur(val idDispo:Int, val idTuteur:Int, val jour: Int, val mois: Int, val annee: Int, val heure: Int, val minute: Int) {
+class DispoTuteur(val idDispo:Int, val idTuteur:Int, var reserver:Boolean, val jour: Int, val mois: Int, val annee: Int, val heure: Int, val minute: Int) {
 
     override fun toString(): String {
         return "$jour-$mois-$annee $heure:$minute" //(jj-mm-aaaa hh:mm)
